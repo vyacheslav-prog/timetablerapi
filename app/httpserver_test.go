585ba3a957c14a00d30b7b7374fa2c194e2e6e6d
vyacheslav <vyacheslav.soynov@gmail.com>
2024-12-03 08:53:13 +0700

    test: response 404 error for unknown path

--- app/httpserver_test.go
+++ app/httpserver_test.go
@@ -18,3 +18,17 @@ func TestListens8080PortForHttpServer(t *testing.T) {
 		t.Errorf("Result for GET / must be [%v], actual is [%v]", http.StatusOK, resp.Status)
 	}
 }
+
+func TestMissesUnknownPathWith404Status(t *testing.T) {
+	go main()
+	time.Sleep(1 * time.Second)
+	path := "/some_unknown_path"
+	resp, err := http.Get("http://localhost:8080" + path)
+	if err != nil {
+		t.Fatalf("Could not reach to server: %v", err)
+	}
+	defer resp.Body.Close()
+	if expected := http.StatusNotFound; expected != resp.StatusCode {
+		t.Errorf("Result for GET [%v] must be [%v], actual is [%v]", path, expected, resp.Status)
+	}
+}
