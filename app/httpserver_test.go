00b28b6325b28d48447fe9ac6f5028ba04eddc8f
vyacheslav <vyacheslav.soynov@gmail.com>
2024-12-05 09:08:27 +0700

    test: use a httptest for response writting

--- app/httpserver_test.go
+++ app/httpserver_test.go
@@ -2,31 +2,25 @@ package main
 
 import (
 	"net/http"
+	"net/http/httptest"
 	"testing"
-	"time"
 )
 
 func TestListens8080PortForHttpServer(t *testing.T) {
-	time.Sleep(1 * time.Second)
-	resp, err := http.Get("http://localhost:8080/")
-	if err != nil {
-		t.Fatalf("Could not reach to server: %v", err)
-	}
-	defer resp.Body.Close()
+	s := newServer()
+	req, w := httptest.NewRequest("GET", "http://localhost:8080/"), httptest.NewRecorder()
+	s.ServeHTTP(w, req)
+	resp := w.Result()
 	if http.StatusOK != resp.StatusCode {
 		t.Errorf("Result for GET / must be [%v], actual is [%v]", http.StatusOK, resp.Status)
 	}
 }
 
 func TestMissesUnknownPathWith404Status(t *testing.T) {
-	go main()
-	time.Sleep(1 * time.Second)
-	url := "http://localhost:8080/some_unknown_path"
-	resp, err := http.Get(url)
-	if err != nil {
-		t.Fatalf("Could not reach to server: %v", err)
-	}
-	defer resp.Body.Close()
+	s, url := newServer(), "http://localhost:8080/some_unknown_path"
+	req, w := httptest.NewRequest("GET", url), httptest.NewRecorder()
+	s.ServeHTTP(w, req)
+	resp := w.Result()
 	if expected := http.StatusNotFound; expected != resp.StatusCode {
 		t.Errorf("Result for GET [%v] must be [%v], actual is [%v]", url, expected, resp.Status)
 	}
