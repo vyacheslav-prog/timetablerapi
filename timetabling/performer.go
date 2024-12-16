a083ca99cb00e641a4831f21b96a4d3440812751
vyacheslav <vyacheslav.soynov@gmail.com>
2024-12-16 07:46:25 +0700

    feat: give first a not busy period for available

--- timetabling/performer.go
+++ timetabling/performer.go
@@ -1,9 +1,14 @@
 package timetabling
 
-type performer struct{}
+type performer struct {
+	emptyPeriods []period
+}
 
 type period struct{}
 
 func (p *performer) findAvailablePeriod() *period {
+	if p.emptyPeriods != nil {
+		return &p.emptyPeriods[0]
+	}
 	return nil
 }
