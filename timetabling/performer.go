03604ad04651746d7a3e75fc689f8a5ea36e84c7
vyacheslav <vyacheslav.soynov@gmail.com>
2024-12-24 09:43:51 +0700

    feat: find an available period by request with period

--- timetabling/performer.go
+++ timetabling/performer.go
@@ -6,7 +6,9 @@ type performer struct {
 
 func (p *performer) findAvailablePeriod(r *period) *period {
 	if p.emptyPeriods != nil {
-		return &p.emptyPeriods[0]
+		if firstPeriod := p.emptyPeriods[0]; r == nil || (firstPeriod.from <= r.from && r.to <= firstPeriod.to) {
+			return &p.emptyPeriods[0]
+		}
 	}
 	return nil
 }
