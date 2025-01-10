006f99301a312a3291effe9af7ad0597d6185206
vyacheslav <vyacheslav.soynov@gmail.com>
2025-01-10 08:09:11 +0700

    feat: view board title for performer

--- overview/performerboard.go
+++ overview/performerboard.go
@@ -1,6 +1,7 @@
 package overview
 
 type performer struct {
+	name      string
 	openSlots []slot
 }
 
@@ -10,4 +11,8 @@ type slot struct {
 
 func (p *performer) getSlots() []slot {
 	return p.openSlots
+}
+
+func (p *performer) viewBoardTitle() string {
+	return "Board for " + p.name
 }
--- overview/performerboard_test.go
+++ overview/performerboard_test.go
@@ -11,7 +11,7 @@ func TestShowsPerformerBoardForNoSlots(t *testing.T) {
 }
 
 func TestShowsPerformerBoardWithOpenSlots(t *testing.T) {
-	p := &performer{[]slot{{"09:00", "10:00"}, {"10:00", "11:00"}}}
+	p := &performer{"", []slot{{"09:00", "10:00"}, {"10:00", "11:00"}}}
 	result := p.getSlots()
 	if expectedFirstFrom := "09:00"; len(result) != 2 || result[0].from != expectedFirstFrom {
 		t.Errorf("Result must have two slots with first slot on [%v], actual slots is [%v]", expectedFirstFrom, result)
@@ -20,8 +20,8 @@ func TestShowsPerformerBoardWithOpenSlots(t *testing.T) {
 
 func TestShowsPerformerBoardWithPersonalTitle(t *testing.T) {
 	pn := "John Doe"
-	p := &performer{pn}
-	result := p.viewBoard()
+	p := &performer{pn, []slot{}}
+	result := p.viewBoardTitle()
 	if expected := "Board for John Doe"; expected != result {
 		t.Errorf("Board title must be [%v] for performer [%v], actual is [%v]", expected, pn, result)
 	}
