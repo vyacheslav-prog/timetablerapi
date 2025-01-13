2a030f34f1917da5a7d2022aca648e0b927c16b5
vyacheslav <vyacheslav.soynov@gmail.com>
2025-01-13 09:09:55 +0700

    test: fetch an empty performer board from repo

---
+++ app/services_test.go
@@ -0,0 +1,11 @@
+package main
+
+import "testing"
+
+func TestFetchsPerformerBoard(t *testing.T) {
+	sut := newOverviewRepo()
+	result := sut.fetchForPerformer()
+	if nil != result {
+		t.Errorf("Result must be nil for empty performer request")
+	}
+}
