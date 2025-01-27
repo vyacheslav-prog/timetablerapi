382eebdaee9517287037465b63496cb6045f4cc4
vyacheslav <vyacheslav.soynov@gmail.com>
2025-01-27 08:47:48 +0700

    test: find a performer board by id

--- app/overview_test.go
+++ app/overview_test.go
@@ -9,3 +9,12 @@ func TestFetchsNoPerformerBoardForEmptyRequest(t *testing.T) {
 		t.Errorf("Result must be nil for empty performer request")
 	}
 }
+
+func TestFetchsPerformerBoardByIdentity(t *testing.T) {
+	id := "2861ff45-526f-4618-9b7a-09e581cb2113"
+	sut, isSown := newOverviewRepo(), seedFakePerformerBoard(id)
+	result := sut.fetchPerformerBoard(id)
+	if nil == result {
+		t.Errorf("Result must be not nil for [%v] performer board id, actual is [%v]", id, result)
+	}
+}
