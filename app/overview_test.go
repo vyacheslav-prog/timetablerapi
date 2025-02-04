0a6a3b142a2705f8a6918a1bbd1824c9b312bb8e
vyacheslav <vyacheslav.soynov@gmail.com>
2025-02-04 07:24:05 +0700

    test: connect to dev database

--- app/overview_test.go
+++ app/overview_test.go
@@ -1,6 +1,10 @@
 package main
 
-import "testing"
+import (
+	"database/sql"
+	"os"
+	"testing"
+)
 
 func TestFetchsNoPerformerBoardForEmptyRequest(t *testing.T) {
 	sut := newOverviewRepo()
@@ -11,7 +15,12 @@ func TestFetchsNoPerformerBoardForEmptyRequest(t *testing.T) {
 }
 
 func TestFetchsPerformerBoardByIdentity(t *testing.T) {
-	id := "2861ff45-526f-4618-9b7a-09e581cb2113"
+	connStr, id := os.Getenv("DATABASE_URL"), "2861ff45-526f-4618-9b7a-09e581cb2113"
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		t.Fatalf("Failed to connect to database [%v]", connStr)
+	}
+	defer db.Close()
 	sut, isSown := newOverviewRepo(), seedFakePerformerBoard(id)
 	result := sut.fetchPerformerBoard(id)
 	if nil == result {
