1cadcd507c2d988f91ec012ef4cb107a4fa0a508
vyacheslav <vyacheslav.soynov@gmail.com>
2025-02-11 08:59:35 +0700

    chore: define a sql schema for performer board

--- app/overview.go
+++ app/overview.go
@@ -3,6 +3,13 @@ package main
 type overviewRepo struct {
 }
 
+const performerBoardsSchema = `
+	create table if not exists performer_boards (
+		id text primary key,
+		created_at datetime default current_timestamp,
+	);
+`
+
 func (r *overviewRepo) fetchPerformerBoard(id string) *int {
 	return nil
 }
