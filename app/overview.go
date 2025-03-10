66e27f825f24fef5d99184267e0040f061cb1335
vyacheslav <vyacheslav.soynov@gmail.com>
2025-03-10 08:26:01 +0700

    refactor: improve a readability for errors

--- app/overview.go
+++ app/overview.go
@@ -28,24 +28,24 @@ func newOverviewRepo(ctx context.Context, db *sql.DB) (*overviewRepo, error) {
 	}
 	tx, err := db.Begin()
 	if err != nil {
-		return nil, fmt.Errorf("init transaction is failed: [%w]", err)
+		return nil, fmt.Errorf("init a migration transaction is failed: %w", err)
 	}
 	defer tx.Rollback()
 	existsRow := tx.QueryRowContext(ctx, "select count(*) from information_schema.tables where table_type = 'BASE TABLE' and table_name = 'performer_boards';")
 	var tableExists int
 	err = existsRow.Scan(&tableExists)
 	if err != nil {
-		return nil, fmt.Errorf("check table existence is failed: [%w]", err)
+		return nil, fmt.Errorf("check table existence is failed: %w", err)
 	}
 	if 0 == tableExists {
 		_, err = tx.ExecContext(ctx, performerBoardsSchema)
 		if err != nil {
-			return nil, fmt.Errorf("create schema for table is failed: [%w]", err)
+			return nil, fmt.Errorf("create schema for table is failed: %w", err)
 		}
 	}
 	err = tx.Commit()
 	if err != nil {
-		return nil, fmt.Errorf("init transaction is failed: [%w]", err)
+		return nil, fmt.Errorf("commit a migration transaction is failed: %w", err)
 	}
 	return &overviewRepo{db: db}, nil
 }
