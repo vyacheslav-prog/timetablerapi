467d7db727f8e1967be4c7807fc79829679836c6
vyacheslav <vyacheslav.soynov@gmail.com>
2025-03-31 07:20:30 +0700

    chore: name for http stubs the same

--- app/httpserver_test.go
+++ app/httpserver_test.go
@@ -18,9 +18,9 @@ func newStubbedServer(s *services) *http.ServeMux {
 
 func TestListens8080PortForHttpServer(t *testing.T) {
 	s := newStubbedServer(nil)
-	req, w := httptest.NewRequest("GET", "http://localhost:8080/", nil), httptest.NewRecorder()
-	s.ServeHTTP(w, req)
-	resp := w.Result()
+	req, rr := httptest.NewRequest("GET", "http://localhost:8080/", nil), httptest.NewRecorder()
+	s.ServeHTTP(rr, req)
+	resp := rr.Result()
 	if http.StatusOK != resp.StatusCode {
 		t.Errorf("Result for GET / must be [%v], actual is [%v]", http.StatusOK, resp.Status)
 	}
@@ -28,9 +28,9 @@ func TestListens8080PortForHttpServer(t *testing.T) {
 
 func TestMissesUnknownPathWith404Status(t *testing.T) {
 	s, url := newStubbedServer(nil), "/some_unknown_path"
-	req, w := httptest.NewRequest("GET", url, nil), httptest.NewRecorder()
-	s.ServeHTTP(w, req)
-	resp := w.Result()
+	req, rr := httptest.NewRequest("GET", url, nil), httptest.NewRecorder()
+	s.ServeHTTP(rr, req)
+	resp := rr.Result()
 	if expected := http.StatusNotFound; expected != resp.StatusCode {
 		t.Errorf("Result for GET [%v] must be [%v], actual is [%v]", url, expected, resp.Status)
 	}
