b1cf6a97aa9cf0b534fd5f82471e50e60213bc63
vyacheslav <vyacheslav.soynov@gmail.com>
2025-05-07 07:59:34 +0700

    test: plan a single task for single available performer

--- timetabling/planner_test.go
+++ timetabling/planner_test.go
@@ -8,3 +8,11 @@ func TestPlansZeroSlotsForNoPerformers(t *testing.T) {
 		t.Errorf("Result must be zero slots for no performers, actual is [%v]", result)
 	}
 }
+
+func TestPlansFirstSlotForSingleTaskAndSinglePerformer(t *testing.T) {
+	t, p := []task{task{"discuss nature"}}, []performer{performer{[]period{period{"06:00", "07:00"}}}}
+	result := plan(p, t)
+	if 1 != len(result) {
+		t.Errorf("Result must be a single job for single available performer, actual is [%v]", result)
+	}
+}
