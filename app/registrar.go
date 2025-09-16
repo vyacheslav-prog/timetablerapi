ffa170c4978f27babe501e1cb7fa8dbed2162c9e
vyacheslav <vyacheslav.soynov@gmail.com>
2025-09-16 07:49:16 +0700

    chore: make a new schema for performers storage

--- app/registrar.go
+++ app/registrar.go
@@ -2,6 +2,14 @@ package main
 
 import "database/sql"
 
+const performersSchema = `
+	create table if not exists performers (
+		id text primary key,
+		created_at timestamp default current_timestamp,
+		name text
+	);
+`
+
 type registrarRepo struct {
 	db *sql.DB
 }
