ee0fd7e633833114693c3638e66b744827fa2923
vyacheslav <vyacheslav.soynov@gmail.com>
2025-10-06 09:13:27 +0700

    fix: detect a err making for overview repo

--- app/overview.go
+++ app/overview.go
@@ -29,28 +29,43 @@ func (r overviewRepo) FetchPerformerBoard(id string) (result *overview.Performer
 	return
 }
 
-func newOverviewRepo(ctx context.Context, db *sql.DB) (*overviewRepo, error) {
+func newOverviewRepo(ctx context.Context, db *sql.DB) (repo *overviewRepo, err error) {
 	if db == nil {
-		return nil, fmt.Errorf("not connection for server")
+		err = fmt.Errorf("not connection for server")
+		return
 	}
 	tx, txBeginErr := db.Begin()
 	if txBeginErr != nil {
-		return nil, fmt.Errorf("init a migration transaction is failed: %w", txBeginErr)
+		err = fmt.Errorf("init a migration transaction is failed: %w", txBeginErr)
+		return
 	}
-	defer tx.Rollback()
+	defer func() {
+		if txErr := tx.Rollback(); txErr != nil {
+			txErr = fmt.Errorf("failed rollack for overview repo: %v", txErr)
+			if err != nil {
+				err = fmt.Errorf("%v, also %v", err, txErr)
+			} else {
+				err = txErr
+			}
+		}
+	}()
 	existsRow := tx.QueryRowContext(ctx, "select count(*) from information_schema.tables where table_type = 'BASE TABLE' and table_name = 'performer_boards';")
 	var tableExists int
 	if existenceErr := existsRow.Scan(&tableExists); existenceErr != nil {
-		return nil, fmt.Errorf("check table existence is failed: %w", existenceErr)
+		err = fmt.Errorf("check table existence is failed: %w", existenceErr)
+		return
 	}
 	if tableExists == 0 {
 		_, createSchemaErr := tx.ExecContext(ctx, performerBoardsSchema)
 		if createSchemaErr != nil {
-			return nil, fmt.Errorf("create schema for table is failed: %w", createSchemaErr)
+			err = fmt.Errorf("create schema for table is failed: %w", createSchemaErr)
+			return
 		}
 	}
 	if txCommitErr := tx.Commit(); txCommitErr != nil {
-		return nil, fmt.Errorf("commit a migration transaction is failed: %w", txCommitErr)
+		err = fmt.Errorf("commit a migration transaction is failed: %w", txCommitErr)
+		return
 	}
-	return &overviewRepo{db: db}, nil
+	repo = &overviewRepo{db: db}
+	return
 }
