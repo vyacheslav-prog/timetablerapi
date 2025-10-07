1636951adade9f8ef42b1e2c2c503f1416fff8ce
vyacheslav <vyacheslav.soynov@gmail.com>
2025-10-07 09:10:38 +0700

    fix: detect a err making for registar repo

--- app/registrar.go
+++ app/registrar.go
@@ -22,28 +22,43 @@ func (rr registrarRepo) SaveAndIdentifyPerformer(name string) (string, error) {
 	return "p1", nil
 }
 
-func newRegistrarRepo(ctx context.Context, db *sql.DB) (*registrarRepo, error) {
+func newRegistrarRepo(ctx context.Context, db *sql.DB) (repo *registrarRepo, err error) {
 	if db == nil {
-		return nil, fmt.Errorf("not connection for server")
+		err = fmt.Errorf("not connection for server")
+		return
 	}
 	tx, txBeginErr := db.Begin()
 	if txBeginErr != nil {
-		return nil, fmt.Errorf("init a migration transaction is failed: %w", txBeginErr)
+		err = fmt.Errorf("init a migration transaction is failed: %w", txBeginErr)
+		return
 	}
-	defer tx.Rollback()
+	defer func() {
+		if txErr := tx.Rollback(); txErr != nil {
+			txErr = fmt.Errorf("failed for registrar repo: %v", txErr)
+			if err != nil {
+				err = fmt.Errorf("%v, also %v", err, txErr)
+			} else {
+				err = txErr
+			}
+		}
+	}()
 	existsRow := tx.QueryRowContext(ctx, "select count(*) from information_schema.tables where table_type = 'BASE TABLE' and table_name = 'performers';")
 	var tableExists int
 	if checkTableErr := existsRow.Scan(&tableExists); checkTableErr != nil {
-		return nil, fmt.Errorf("check table existence is failed: %w", checkTableErr)
+		err = fmt.Errorf("check table existence is failed: %w", checkTableErr)
+		return
 	}
 	if tableExists == 0 {
 		_, migrateErr := tx.ExecContext(ctx, performersSchema)
 		if migrateErr != nil {
-			return nil, fmt.Errorf("create schema for table is failed: %w", migrateErr)
+			err = fmt.Errorf("create schema for table is failed: %w", migrateErr)
+			return
 		}
 	}
 	if txCommitErr := tx.Commit(); txCommitErr != nil {
-		return nil, fmt.Errorf("commit a migration transaction is failed: %w", txCommitErr)
+		err = fmt.Errorf("commit a migration transaction is failed: %w", txCommitErr)
+		return
 	}
-	return &registrarRepo{db: db}, nil
+	repo = &registrarRepo{db: db}
+	return
 }
