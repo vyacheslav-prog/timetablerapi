f2dbd21fb4a68e9a04c4edfc8660030b922e0f05
vyacheslav <vyacheslav.soynov@gmail.com>
2025-12-15 08:54:21 +0700

    refactor: declare internal services for registrar

---
+++ internal/services/registrar.go
@@ -0,0 +1,30 @@
+package services
+
+import (
+	"context"
+	"database/sql"
+)
+
+const performersSchema = `
+	create table if not exists performers (
+		id text primary key,
+		created_at timestamp default current_timestamp,
+		name text
+	);
+`
+
+type registrarRepo struct {
+	db *sql.DB
+}
+
+func (rr registrarRepo) SaveAndIdentifyPerformer(name string) (string, error) {
+	return "p1", nil
+}
+
+func newRegistrarRepo(ctx context.Context, db *sql.DB) (*registrarRepo, error) {
+	err := execSQLMigrationByScheme(performersSchema, "performers", ctx, db)
+	if err != nil {
+		return nil, err
+	}
+	return &registrarRepo{db: db}, nil
+}
