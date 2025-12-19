0d91aebece273226527a2b86b2006c68514fc507
vyacheslav <vyacheslav.soynov@gmail.com>
2025-12-19 08:54:31 +0700

    fix: restore services initializing

--- internal/services/services.go
+++ internal/services/services.go
@@ -1,3 +1,52 @@
 package services
 
-func NewServices() {}
+import (
+	"context"
+	"database/sql"
+	"errors"
+	"os"
+
+	_ "github.com/lib/pq"
+	"timetablerapi/overview"
+	"timetablerapi/registrar"
+)
+
+type overviewService interface {
+	ViewPerformerBoard(context.Context, string) (string, error)
+}
+
+type registrarService interface {
+	AddPerformer(string) (string, error)
+}
+
+type services struct {
+	overview  overviewService
+	registrar registrarService
+}
+
+var (
+	errInitServices = errors.New("failed init services")
+)
+
+func NewServices(ctx context.Context) (*services, error) {
+	dbConn, dbMode := os.Getenv("DATABASE_URL"), os.Getenv("DATABASE_MODE")
+	db, openErr := sql.Open(dbMode, dbConn)
+	if openErr != nil {
+		return nil, errors.Join(errInitServices, openErr)
+	}
+	if pingErr := db.PingContext(ctx); pingErr != nil {
+		return nil, errors.Join(errInitServices, pingErr)
+	}
+	or, orErr := newOverviewRepo(ctx, db)
+	if orErr != nil {
+		return nil, orErr
+	}
+	rr, rrErr := newRegistrarRepo(ctx, db)
+	if rrErr != nil {
+		return nil, rrErr
+	}
+	return &services{
+		overview.Overview{Repo: or},
+		registrar.Registrar{Repo: rr},
+	}, nil
+}
