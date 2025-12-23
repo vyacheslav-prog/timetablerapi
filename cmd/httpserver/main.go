8963333ddfbe7d1116a1f614b12d62f85bf1890d
vyacheslav <vyacheslav.soynov@gmail.com>
2025-12-23 08:48:47 +0700

    refactor: register a http handlers for module

--- cmd/httpserver/main.go
+++ cmd/httpserver/main.go
@@ -9,6 +9,7 @@ import (
 	"os"
 	"time"
 
+	"timetablerapi/internal/httpserver"
 	"timetablerapi/internal/services"
 )
 
@@ -27,8 +28,7 @@ func main() {
 		log.Println("failed initialization for services:", initErr)
 		os.Exit(1)
 	}
-	mux := http.NewServeMux()
-	registerHandlers(mux, srvs)
+	mux := httpserver.NewMux(srvs)
 	httpSrv := &http.Server{
 		Addr:              srvAddr,
 		ReadHeaderTimeout: srvReadHeaderTimeout,
@@ -40,58 +40,4 @@ func main() {
 		log.Println("failed serve for http-server:", serveErr)
 	}
 	log.Println("timetablerapi server for http listen 8080 port")
-}
-
-type performerCreatingRequest struct {
-	Name string `json:"name"`
-}
-
-func handleAddPerformer(s services.RegistrarService, w http.ResponseWriter, r *http.Request) {
-	body, readBodyErr := io.ReadAll(r.Body)
-	if readBodyErr != nil {
-		log.Print("failed body read:", readBodyErr)
-	}
-	var data performerCreatingRequest
-	if unmarshallErr := json.Unmarshal(body, &data); unmarshallErr != nil {
-		log.Print("failed body decoding:", unmarshallErr)
-	}
-	res, regErr := s.AddPerformer(data.Name)
-	if regErr != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		writeResponse(w, []byte(regErr.Error()))
-		return
-	}
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusCreated)
-	writeResponse(w, []byte(res))
-}
-
-func handleViewPerformerBoard(s services.OverviewService, w http.ResponseWriter, r *http.Request) {
-	res, ovErr := s.ViewPerformerBoard(r.Context(), r.PathValue("boardId"))
-	if ovErr != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		writeResponse(w, []byte(ovErr.Error()))
-		return
-	}
-	w.Header().Set("Content-Type", "application/json")
-	writeResponse(w, []byte(res))
-}
-
-func registerHandlers(mux *http.ServeMux, s *services.Services) {
-	mux.HandleFunc("/{$}", func(w http.ResponseWriter, r *http.Request) {
-		writeResponse(w, []byte("ok"))
-	})
-	mux.HandleFunc("GET /performer-boards/{boardId}", func(w http.ResponseWriter, r *http.Request) {
-		handleViewPerformerBoard(s.Overview, w, r)
-	})
-	mux.HandleFunc("POST /performers", func(w http.ResponseWriter, r *http.Request) {
-		handleAddPerformer(s.Registrar, w, r)
-	})
-}
-
-func writeResponse(w http.ResponseWriter, b []byte) {
-	_, respErr := w.Write(b)
-	if respErr != nil {
-		log.Print("can not to write a response:", respErr)
-	}
 }
--- internal/httpserver/mux.go
+++ internal/httpserver/mux.go
@@ -1,7 +1,66 @@
 package httpserver
 
-import "net/http"
+import (
+	"net/http"
+	"timetablerapi/internal/services"
+)
 
-func NewMux() *http.ServeMux {
-	return nil
+func NewMux(srvs *services.Services) *http.ServeMux {
+	mux := http.NewServeMux()
+	registerHandlers(mux, srvs)
+	return mux
+}
+
+type performerCreatingRequest struct {
+	Name string `json:"name"`
+}
+
+func handleAddPerformer(s services.RegistrarService, w http.ResponseWriter, r *http.Request) {
+	body, readBodyErr := io.ReadAll(r.Body)
+	if readBodyErr != nil {
+		log.Print("failed body read:", readBodyErr)
+	}
+	var data performerCreatingRequest
+	if unmarshallErr := json.Unmarshal(body, &data); unmarshallErr != nil {
+		log.Print("failed body decoding:", unmarshallErr)
+	}
+	res, regErr := s.AddPerformer(data.Name)
+	if regErr != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		writeResponse(w, []byte(regErr.Error()))
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	writeResponse(w, []byte(res))
+}
+
+func handleViewPerformerBoard(s services.OverviewService, w http.ResponseWriter, r *http.Request) {
+	res, ovErr := s.ViewPerformerBoard(r.Context(), r.PathValue("boardId"))
+	if ovErr != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		writeResponse(w, []byte(ovErr.Error()))
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	writeResponse(w, []byte(res))
+}
+
+func registerHandlers(mux *http.ServeMux, s *services.Services) {
+	mux.HandleFunc("/{$}", func(w http.ResponseWriter, r *http.Request) {
+		writeResponse(w, []byte("ok"))
+	})
+	mux.HandleFunc("GET /performer-boards/{boardId}", func(w http.ResponseWriter, r *http.Request) {
+		handleViewPerformerBoard(s.Overview, w, r)
+	})
+	mux.HandleFunc("POST /performers", func(w http.ResponseWriter, r *http.Request) {
+		handleAddPerformer(s.Registrar, w, r)
+	})
+}
+
+func writeResponse(w http.ResponseWriter, b []byte) {
+	_, respErr := w.Write(b)
+	if respErr != nil {
+		log.Print("can not to write a response:", respErr)
+	}
 }
