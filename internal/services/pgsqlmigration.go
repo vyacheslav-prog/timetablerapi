e8154d52aabe8b8f06a632ddb5c92ab64b37cac3
vyacheslav <vyacheslav.soynov@gmail.com>
2026-02-05 08:48:28 +0700

    chore: migrate for sqlite3 driver db

--- internal/services/pgsqlmigration.go
+++ internal/services/pgsqlmigration.go
@@ -47,7 +47,52 @@ func execPgSQLMigrationByScheme(ctx context.Context, scm, tbl string, db *sql.DB
 	return nil
 }
 
+func execSQLite3MigrationByScheme(ctx context.Context, scm, tbl string, db *sql.DB) (err error) {
+	if db == nil {
+		err = errMigrationNotConnection
+		return
+	}
+	tx, txBeginErr := db.BeginTx(ctx, nil)
+	if txBeginErr != nil {
+		err = errors.Join(errMigrationTransactionIsFailed, txBeginErr)
+		return
+	}
+	defer func() {
+		if txErr := tx.Rollback(); txErr != nil {
+			txErr = errors.Join(errMigrationTransactionIsFailed, txErr)
+			if err != nil {
+				err = errors.Join(err, txErr)
+			} else {
+				err = txErr
+			}
+		}
+	}()
+	if err = checkSQLite3TableExistenceOrCreate(ctx, scm, tbl, tx); err != nil {
+		return
+	}
+	if txCommitErr := tx.Commit(); txCommitErr != nil {
+		err = errors.Join(errMigrationTransactionIsFailed, txCommitErr)
+		return
+	}
+	return nil
+}
+
 func checkPgSQLTableExistenceOrCreate(ctx context.Context, scm, tbl string, tx *sql.Tx) error {
+	existsRow := tx.QueryRowContext(ctx, pgCountTableByNameQuery, tbl)
+	var tableExists int
+	if checkTableErr := existsRow.Scan(&tableExists); checkTableErr != nil {
+		return errors.Join(errMigrationCheckTable, checkTableErr)
+	}
+	if tableExists == 0 {
+		_, migrateErr := tx.ExecContext(ctx, scm)
+		if migrateErr != nil {
+			return errors.Join(errMigrationCreateScheme, migrateErr)
+		}
+	}
+	return nil
+}
+
+func checkSQLite3TableExistenceOrCreate(ctx context.Context, scm, tbl string, tx *sql.Tx) error {
 	existsRow := tx.QueryRowContext(ctx, pgCountTableByNameQuery, tbl)
 	var tableExists int
 	if checkTableErr := existsRow.Scan(&tableExists); checkTableErr != nil {
