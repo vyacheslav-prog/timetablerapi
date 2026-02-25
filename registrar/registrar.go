517b8231e31f96d00fa4f4f2798b95291dadc9e9
vyacheslav <vyacheslav.soynov@gmail.com>
2026-02-25 08:43:46 +0700

    refactor: simplify an error handling

--- registrar/registrar.go
+++ registrar/registrar.go
@@ -14,22 +14,21 @@ type Registrar struct {
 }
 
 var (
-	errAddPerfomer = errors.New("unable to add a performer")
-	errAddPeriod   = errors.New("unable to add a period")
+	errRegistrar = errors.New("registrar error is occured")
 )
 
 func (r Registrar) AddLayout(mode string) (string, error) {
-	return "", fmt.Errorf("not implemented")
+	return "", fmt.Errorf("%w: not implemented", errRegistrar)
 }
 
 func (r Registrar) AddPerformer(name string) (string, error) {
 	identity, err := r.Repo.SaveAndIdentifyPerformer(name)
 	if err != nil {
-		return "", errors.Join(errAddPerfomer, err)
+		return "", errors.Join(errRegistrar, err)
 	}
 	return identity, nil
 }
 
 func (r Registrar) AddPeriod(from, to string) (string, error) {
-	return "", fmt.Errorf("%w: not implemented", errAddPeriod)
+	return "", fmt.Errorf("%w: not implemented", errRegistrar)
 }
