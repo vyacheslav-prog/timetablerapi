239f12991c39276be72bf9fa1e7c258350aa9c11
vyacheslav <vyacheslav.soynov@gmail.com>
2026-03-06 09:09:06 +0700

    feat: declare scheme for tasks

--- internal/services/registrar.go
+++ internal/services/registrar.go
@@ -19,6 +19,16 @@ const performersSchema = `
 	);
 `
 
+const taskSchema = `
+	create table if not exists tasks (
+		id text primary key,
+		created_at timestamp default current_timestamp,
+		name text,
+		from text,
+		to text
+	);
+`
+
 type registrarRepo struct {
 	db *sql.DB
 }
@@ -31,12 +41,20 @@ func (rr registrarRepo) SaveAndIdentifyPerformer(name string) (string, error) {
 	return "p1", nil
 }
 
+func (rr registrarRepo) SaveAndIdentifyTask(name, from, to string) (string, error) {
+	return "t1", nil
+}
+
 func newRegistrarRepo(ctx context.Context, db *sql.DB, dm *dbMigrate) (*registrarRepo, error) {
 	err := dm.byScheme(ctx, performersSchema, "performers")
 	if err != nil {
 		return nil, err
 	}
 	err = dm.byScheme(ctx, layoutsSchema, "layouts")
+	if err != nil {
+		return nil, err
+	}
+	err = dm.byScheme(ctx, taskSchema, "tasks")
 	if err != nil {
 		return nil, err
 	}
