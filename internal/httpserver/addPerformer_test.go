c570b9f4506c0dacd8a864259bf13fb91cf5c087
vyacheslav <vyacheslav.soynov@gmail.com>
2026-04-07 08:50:58 +0700

    chore: stub a registrar service for testing

--- internal/httpserver/addPerformer_test.go
+++ internal/httpserver/addPerformer_test.go
@@ -2,6 +2,7 @@ package httpserver
 
 import (
 	"context"
+	"net/http"
 	"testing"
 	"timetablerapi/internal/services"
 	"timetablerapi/registrar"
@@ -10,10 +11,13 @@ import (
 func TestEmptyAddPerformerRequest(t *testing.T) {
 	rg := struct {
 		AddPerformer func(context.Context, registrar.Performer) (string, error)
-	} {
+	}{
 		AddPerformer: func(_ context.Context, _ registrar.Performer) (string, error) {
 			return "", nil
 		},
 	}
-	NewMux(&services.Services{Registrar: rg})
+	mux := http.NewServeMux()
+	registerHandlers(mux, &services.Services{Registrar: rg})
+	req := http.NewRequest("POST", "/performers")
+	mux.ServeHTTP()
 }
