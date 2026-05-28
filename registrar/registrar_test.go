02ec7b09acb4717ef981617843c7f9946814a689
vyacheslav <vyacheslav.soynov@gmail.com>
2026-05-28 09:02:01 +0700

    chore: stub a registrar repo for testing

--- registrar/registrar_test.go
+++ registrar/registrar_test.go
@@ -1,11 +1,28 @@
 package registrar
 
-import "testing"
+import (
+	"context"
+	"testing"
+)
+
+type stubRepository struct { }
+
+func (r stubRepository) SaveAndIdentifyLayout(context.Context, string) (string, error) {
+	return "", nil
+}
+
+func (r stubRepository) SaveAndIdentifyPerformer(context.Context, string) (string, error) {
+	return "", nil
+}
+
+func (r stubRepository) SaveAndIdentifyTask(context.Context, string, string, string) (string, error) {
+	return "", nil
+}
 
 func TestNoFiresEventWhenPerformerAddingIsFail(t *testing.T) {
-	reg := &Registrar{}
+	reg := &Registrar{Repo: stubRepository{}}
 	_, err := reg.AddPerformer(t.Context(), Performer{})
-	if err != nil {
+	if err == nil {
 		t.Error("failed try must return error, given nil")
 		return
 	}
