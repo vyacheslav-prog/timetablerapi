c639900fddb836b7ad3ef132d49fd00f6e764d35
vyacheslav <vyacheslav.soynov@gmail.com>
2026-05-29 08:49:32 +0700

    test: stub error result for registrar

--- registrar/registrar_test.go
+++ registrar/registrar_test.go
@@ -2,25 +2,28 @@ package registrar
 
 import (
 	"context"
+	"errors"
 	"testing"
 )
 
-type stubRepository struct { }
+type stubRepository struct {
+	err error
+}
 
 func (r stubRepository) SaveAndIdentifyLayout(context.Context, string) (string, error) {
-	return "", nil
+	return "", r.err
 }
 
 func (r stubRepository) SaveAndIdentifyPerformer(context.Context, string) (string, error) {
-	return "", nil
+	return "", r.err
 }
 
 func (r stubRepository) SaveAndIdentifyTask(context.Context, string, string, string) (string, error) {
-	return "", nil
+	return "", r.err
 }
 
 func TestNoFiresEventWhenPerformerAddingIsFail(t *testing.T) {
-	reg := &Registrar{Repo: stubRepository{}}
+	reg := &Registrar{Repo: stubRepository{err: errors.New("something wrong")}}
 	_, err := reg.AddPerformer(t.Context(), Performer{})
 	if err == nil {
 		t.Error("failed try must return error, given nil")
