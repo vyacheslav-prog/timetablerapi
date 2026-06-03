48ba2972f007d415828432ca7ffbb538e6a3356d
vyacheslav <vyacheslav.soynov@gmail.com>
2026-06-03 09:11:36 +0700

    test: fire event when task adding is success

--- registrar/registrar_test.go
+++ registrar/registrar_test.go
@@ -43,3 +43,16 @@ func TestFiresEventWhenPerformerAddingIsDone(t *testing.T) {
 		t.Error("registar no contains events when performer is added")
 	}
 }
+
+func TestFiresEventWhenTaskAddingIsDone(t *testing.T) {
+	reg := &Registrar{Repo: stubRepository{}}
+	_, err := reg.AddTask(t.Context(), Task{})
+	if err != nil {
+		t.Error("failed a task adding:", err)
+		return
+	}
+	e := reg.Events()
+	if len(e) == 0 || e[0] != eventTaskAdded {
+		t.Error("registar no contains events when task is added, given:", e)
+	}
+}
