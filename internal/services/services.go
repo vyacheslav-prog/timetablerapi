ddfcc5faad937b5fa9a77a8600fc50358e93c90f
vyacheslav <vyacheslav.soynov@gmail.com>
2026-06-18 08:49:39 +0700

    fix: flush events by registrar

--- internal/services/services.go
+++ internal/services/services.go
@@ -20,6 +20,7 @@ type OverviewService interface {
 type RegistrarService interface {
 	AddPerformer(context.Context, registrar.Performer) (string, error)
 	AddTask(context.Context, registrar.Task) (string, error)
+	Events() []Event
 }
 
 type Services struct {
@@ -58,5 +59,5 @@ func NewServices(ctx context.Context) (*Services, error) {
 }
 
 func (s Services) FlushEvents() error {
-	return nil
+	return s.Events.Collect([]EventsSource{s.Registrar})
 }
