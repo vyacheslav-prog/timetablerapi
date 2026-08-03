46b72dae978421906b15765e155f8ea6d921c85f
vyacheslav <vyacheslav.soynov@gmail.com>
2026-08-03 09:09:22 +0700

    fix: connect to postgres for services

--- internal/services/pgsql.go
+++ internal/services/pgsql.go
@@ -10,7 +10,7 @@ import (
 
 const (
 	countTableByNameQuery = "select count(*) from information_schema.tables where table_type = 'BASE TABLE' and table_name = '$1';"
-	sqlDriver             = "pq"
+	sqlDriver             = "postgres"
 )
 
 func openDBConnect(dsn string) (*sql.DB, error) {
