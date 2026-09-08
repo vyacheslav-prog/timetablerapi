68f40db28fa9a6beb400c664a3a91696d2442be4
vyacheslav <vyacheslav.soynov@gmail.com>
2026-09-08 08:34:46 +0700

    refactor: clarify deps for http handler

--- internal/httpserver/viewPerformerBoard.go
+++ internal/httpserver/viewPerformerBoard.go
@@ -1,12 +1,17 @@
 package httpserver
 
 import (
+	"context"
 	"net/http"
 
-	"timetablerapi/internal/services"
+	"timetablerapi/overview"
 )
 
-func handleViewPerformerBoard(s services.OverviewService, w http.ResponseWriter, r *http.Request) {
+type viewPerformerService interface {
+	ViewPerformerBoard(context.Context, string) (overview.PerformerBoard, error)
+}
+
+func handleViewPerformerBoard(s viewPerformerService, w http.ResponseWriter, r *http.Request) {
 	res, ovErr := s.ViewPerformerBoard(r.Context(), r.PathValue("boardId"))
 	if ovErr != nil {
 		http.Error(w, ovErr.Error(), http.StatusInternalServerError)
