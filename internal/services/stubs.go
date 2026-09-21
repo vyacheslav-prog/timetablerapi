a0cd4b367bd5b572614f10fc2b2689c297d949c6
vyacheslav <vyacheslav.soynov@gmail.com>
2026-09-21 08:46:37 +0700

    refactor: remove a redunant code

--- internal/services/stubs.go
+++ internal/services/stubs.go
@@ -6,16 +6,9 @@ import (
 	"timetablerapi/overview"
 )
 
-type OverviewStub struct {
-}
-
 type OverviewRepoStub struct {
 }
 
 func (s OverviewRepoStub) FetchPerformerBoard(context.Context, string) (result *overview.PerformerBoard, err error) {
-	return overview.NewPerformerBoard("", "", "board"), nil
-}
-
-func (os OverviewStub) ViewPerformerBoard(context.Context, string) (*overview.PerformerBoard, error) {
 	return overview.NewPerformerBoard("", "", "board"), nil
 }
